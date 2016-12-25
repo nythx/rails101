918db68d3a86baffcfea6346d7ff6c9074b2b0c0
mavel <69673480@qq.com>
2016-12-25 17:38:32 +0800

    add validation to title

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -25,9 +25,13 @@ class GroupsController < ApplicationController
     redirect_to groups_path
   end
   def create
-     @group = Group.new(group_params)
-      @group.save
+    @group = Group.new(group_params)
+
+    if @group.save
       redirect_to groups_path
+    else
+      render :new
+    end
   end
   private
 
